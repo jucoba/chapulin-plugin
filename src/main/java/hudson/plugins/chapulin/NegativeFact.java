ceba30cf3d4e495787097880578c3f29066983ad
jucoba <jubel.correa@gmail.com>
2014-01-26 17:26:56 -0500

    Se codigican en html las frases

--- src/main/java/hudson/plugins/chapulin/NegativeFact.java
+++ src/main/java/hudson/plugins/chapulin/NegativeFact.java
@@ -6,17 +6,16 @@ public class NegativeFact implements Fact {
 	private static final Random RANDOM = new Random();
 
 	 private static final String[] FACTS = {         
-         "Calma, Calma, que no panda el cúnico",         
-         "Silencio, Silencio... mis antenitas de vinil están detectando la presencia del enemigo",
+         "Calma, Calma, que no panda el c&uacute;nico",         
+         "Silencio, Silencio... mis antenitas de vinil est&aacute;n detectando la presencia del enemigo",
          "!Oh! y ahora. Quien podrá ayudarnos ? ... Yoooo!!.  El Chapulin Colorado",
-         "Lo hice intencionalmente para comprobar la capacidad de reacción del equipo",
+         "Lo hice intencionalmente para comprobar la capacidad de reacci&oacute;n del equipo",
          "Se aprovechan de mi nobleza",
-         "Lo sospeché desde un principio",
-         "Sí lo arreglo; sí lo arreglo; sí lo arreglo... (¡¡¡Pero ya!!!)",
+         "Lo sospech&eacute; desde un principio",
+         "Si lo arreglo; si lo arreglo; si lo arreglo... (¡¡¡Pero ya!!!)",
          "¡Chanfle!, ¡¡Rechanfle!!, ¡¡¡Recontrachanfle!!!",
          "Lo que no tienes de menso, lo tienes de burro",
-         "Un error lo comete cualquiera... Quinientos errores los comete cualquiera",
-         "¡Me vas a destrozar mi build!"
+         "Un error lo comete cualquiera... Quinientos errores los comete cualquiera"         
          };
 	
 	@Override
--- src/main/java/hudson/plugins/chapulin/PositiveFact.java
+++ src/main/java/hudson/plugins/chapulin/PositiveFact.java
@@ -8,11 +8,11 @@ public class PositiveFact implements Fact {
 
 	private static final String[] FACTS = {
         "No contaban con mi astucia",
-        "Todos mis movimientos están fríamente calculados",        
-        "El Chapulin Colorado no ha sido vencido jamás",
-        "Síganme los buenos",
+        "Todos mis movimientos est&aacute;n fríamente calculados",        
+        "El Chapulin Colorado no ha sido vencido jam&aacute;s",
+        "S&iacute;ganme los buenos",
         "Es exactamente lo que iba yo a hacer",
-        " ¡Eres lo máximo, Chapulín! "
+        " ¡Eres lo m&aacute;ximo, Chapulín! "
         };
 	
 	@Override
